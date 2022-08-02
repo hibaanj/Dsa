2460b7d13e90823311732a934d500175bcce3a88
hibaanj <anjumhiba15@gmail.com>
2022-08-03 02:36:45 +0530

    added queue use cases

---
+++ queue/Program.cs
@@ -0,0 +1,13 @@
+﻿using linkedlistqueue;
+
+Console.WriteLine("Hello World!");
+LinkedListQueue linkedListQueue = new LinkedListQueue();
+linkedListQueue.Enqueue(56);
+linkedListQueue.Enqueue(30);
+linkedListQueue.Enqueue(70);
+linkedListQueue.Display();// this.top=this.top.next
+linkedListQueue.Dequeue();
+linkedListQueue.Dequeue();
+linkedListQueue.Dequeue();
+linkedListQueue.Display();
+Console.ReadKey();
