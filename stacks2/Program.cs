edb27fc1e66b916846921a5c29bca363cd0f50ed
hibaanj <anjumhiba15@gmail.com>
2022-08-03 02:20:48 +0530

    stack peek and pop

---
+++ stacks2/Program.cs
@@ -0,0 +1,13 @@
+﻿using stacks2;
+
+Console.WriteLine("Hello World!");
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Display();
+
+linkedListStack.Peek(); 
+linkedListStack.Pop();
+linkedListStack.IsEmpty();
+linkedListStack.Display();
